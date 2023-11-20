4e2a95c5a30b1821ac6b097892ec0aec030d3b0a
Eric Janson <144372920+emjanson@users.noreply.github.com>
2023-11-20 16:05:22 -0500

    Update challenge9schemata.sql

--- EmployeeSQL/challenge9schemata.sql
+++ EmployeeSQL/challenge9schemata.sql
@@ -23,6 +23,7 @@ CREATE TABLE employees (
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
     dept_no VARCHAR(4) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -30,12 +31,13 @@ CREATE TABLE dept_emp (
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-    emp_no INT NOT NULL,
+    emp_no INT PRIMARY KEY NOT NULL,
     salary INT,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
